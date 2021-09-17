4eb9cff9996dbe9719e17694d2599945cb7769c8
John Dempsey <john.dempsey@csuci.edu>
2021-09-17 11:13:39 +0800

    added img

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -1,4 +1,5 @@
 import React from "react";
+import myimg from "../assets/images/myimg.jpg"
 
 function Intro() {
   return (
@@ -22,7 +23,7 @@ function Intro() {
             <img
               alt="myimage"
               className="myImage"
-              src="https://scontent.fcrk1-1.fna.fbcdn.net/v/t1.6435-9/147454798_5042041179203023_1862199331036992512_n.jpg?_nc_cat=103&ccb=1-3&_nc_sid=09cbfe&_nc_eui2=AeHFJUko907LPxerP_uVzzvhBNjeZwestDUE2N5nB6y0Nbwfq61pHIiwwjXxUM9nSMDNEBISA3MFXpBgCyputGIu&_nc_ohc=QIH_Z2wm6sYAX-Q04LY&_nc_ht=scontent.fcrk1-1.fna&oh=336869968b6ea428ed0072c7c38db6b4&oe=60F74EB6"
+              src={myimg}
             ></img>
           </div>
         </div>
